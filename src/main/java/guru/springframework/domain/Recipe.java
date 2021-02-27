fbaa82cc65cd56e3a8857f67745896ffff84e480
Kristina <kristina.vidakovic10@gmail.com>
2021-02-27 21:08:28 +0100

    added repositories

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -2,6 +2,7 @@ package guru.springframework.domain;
 
 import javax.persistence.*;
 import java.util.Set;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -26,6 +27,20 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name= "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name= "category_id"))
+    private Set<Category> categories;
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
