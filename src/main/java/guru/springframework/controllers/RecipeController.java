af39bc216cb4763a74bf7433a2ff1581f6e30354
Kristina <kristina.vidakovic10@gmail.com>
2021-03-16 14:31:19 +0100

    added error messages using thymeleaf

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -7,8 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 @Slf4j
@@ -41,7 +44,14 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+    public String saveOrUpdate(@Valid @ModelAttribute ("recipe") RecipeCommand command, BindingResult result){
+
+        if(result.hasErrors()){
+            result.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+            return "recipe/recipeform";
+        }
         RecipeCommand savedCommand= recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
@@ -66,17 +76,6 @@ public class RecipeController {
        return mv;
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception){
-        log.error("Handling number format exception");
-        log.error(exception.getMessage());
 
-        ModelAndView mv=new ModelAndView();
-        mv.setViewName("400error");
-        mv.addObject("exception", exception);
-
-        return mv;
-    }
 
 }
